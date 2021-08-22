8b20c19f3831a87b0977ef7db001023ac3373c88
AnthonyTerreros <dterrero@espol.edu.ec>
2021-08-21 23:34:54 -0500

    Creacion de Proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.espol.ed.tresenrayaedg5 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.espol.ed.tresenrayaedg5 to javafx.fxml;
+    exports com.espol.ed.tresenrayaedg5;
+}
