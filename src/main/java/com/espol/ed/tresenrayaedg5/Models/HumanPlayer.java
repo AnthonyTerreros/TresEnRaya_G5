f4384eed1695411f8de62b17ef543cb984784150
lizVergara <lizvcast@espol.edu.ec>
2021-08-22 01:30:53 -0500

    Human Player

---
+++ src/main/java/com/espol/ed/tresenrayaedg5/Models/HumanPlayer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.espol.ed.tresenrayaedg5.Models;
+
+/**
+ *
+ * @author Lizbeth Vergara
+ */
+public class HumanPlayer {
+    
+    public final char jugador;
+    
+    public HumanPlayer(){
+        jugador = 'O';
+    }
+
+    public char getJugador() {
+        return jugador;
+    }
+    
+}
