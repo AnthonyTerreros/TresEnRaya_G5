99a94df1c918a1decb83638d3c610834df0ce8d5
AnthonyTerreros <dterrero@espol.edu.ec>
2021-08-23 00:30:19 -0500

    Creacion de la Interfaz Principal

--- src/main/java/com/espol/ed/tresenrayaedg5/App.java
+++ src/main/java/com/espol/ed/tresenrayaedg5/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("VentanaGame"), 640, 480);
+        scene = new Scene(loadFXML("VentanaPrincipal"), 640, 500);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/espol/ed/tresenrayaedg5/VentanaPrincipalController.java
+++ src/main/java/com/espol/ed/tresenrayaedg5/VentanaPrincipalController.java
@@ -2,7 +2,11 @@ package com.espol.ed.tresenrayaedg5;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 
 /**
  * FXML Controller class
@@ -10,13 +14,37 @@ import javafx.fxml.Initializable;
  * @author dante
  */
 public class VentanaPrincipalController implements Initializable {
-
+    
+    @FXML
+    public Button btnIniciar;
+    
+    @FXML 
+    public RadioButton xPlayer;
+    public RadioButton yPlayer;
+    public RadioButton startHuman;
+    public RadioButton startComputer;
+    
+    
+    public static String primerJugador;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    
+    @FXML
+    public void empezarPartida(ActionEvent e){
+        if(xPlayer.isSelected()){
+            primerJugador = "";
+        }
+        
+        if(yPlayer.isSelected()){
+            
+        }
+        
+    }
+    
     
 }
